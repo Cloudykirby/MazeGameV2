4396855c1f2297eed30e2fc3638a0ee0845e1961
Kirby <kirbyc6@gmail.com>
2021-04-28 15:59:09 -0400

    create loading page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { useState } from "react";
+import Maze from "./Component/Maze";
 
 function App() {
+  const [difficulty, setDifficulty] = useState("Easy");
+  const handleChange = (e) => {
+    setDifficulty(e.target.value);
+  };
+  const handleClick = () => {
+    console.log(difficulty);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Play Game</h1>
+      <p>Choose your difficulty</p>
+      <select
+        className="difficulty_setting"
+        name="difficulty"
+        id="settings"
+        onChange={handleChange}
+      >
+        <option value="Easy">Easy</option>
+        <option value="Medium">Medium</option>
+        <option value="Hard">Hard</option>
+        <option value="Hell">Hell</option>
+      </select>
+      <button>Play Game</button>
     </div>
   );
 }
